d569fa3861ca9d41ed87a029611f0f2aa3d1345e
Osvaldo Alves <oalves.neto@outlook.com>
2025-02-21 11:58:54 -0300

    feat: setup fastify cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,11 @@
 import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
 
 const app = fastify()
+
+app.register(fastifyCors, {
+  origin: true,
+})
 
 app.get('/healthcheck', () => {
   console.log('GET 200 /healthcheck')

14526d2c12861ef5e9209e76f46d5b817d89a1f4
Osvaldo Alves <oalves.neto@outlook.com>
2025-02-21 11:55:46 -0300

    feat: setup server HTTP base config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,12 @@
-console.log('Hello world!')
+import { fastify } from 'fastify'
+
+const app = fastify()
+
+app.get('/healthcheck', () => {
+  console.log('GET 200 /healthcheck')
+  return 'Ok'
+})
+
+app.listen({ port: 3000 }).then(() => {
+  console.log('🚀 HTTP server is running!', 'http://127.0.0.1:3000')
+})

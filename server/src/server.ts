832540a02b0c1770bfe0c48201f8f84c49bcd680
Osvaldo Alves <oalves.neto@outlook.com>
2025-02-21 22:21:44 -0300

    feat: setup swagger docs on project

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,9 +3,12 @@ import { fastifyCors } from '@fastify/cors'
 import {
   validatorCompiler,
   serializerCompiler,
-  ZodTypeProvider
+  ZodTypeProvider,
+  jsonSchemaTransform
 } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -14,6 +17,20 @@ app.setValidatorCompiler(validatorCompiler)
 
 app.register(fastifyCors, {
   origin: true,
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'NLW Connect',
+      version: '0.0.1'
+    }
+  },
+  transform: jsonSchemaTransform
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs'
 })
 
 app.post('/subscribers', {

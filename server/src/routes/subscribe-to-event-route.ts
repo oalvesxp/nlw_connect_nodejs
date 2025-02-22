303ee9a8c5c3932c1dd26d0899c57d00f4124c9a
Osvaldo Alves <oalves.neto@outlook.com>
2025-02-22 07:53:28 -0300

    feat: separate route from server file.

---
+++ server/src/routes/subscribe-to-event-route.ts
@@ -0,0 +1,32 @@
+import z from 'zod'
+import { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+
+export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) => {
+  app.post('/subscribers', {
+    schema: {
+      summary: 'Subscribes someone to the event',
+      tags: ['Subscription'],
+      description: 'Route to subscribes someone to the event',
+      body: z.object({
+        name: z.string(),
+        email: z.string().email()
+      }),
+      response: {
+        201: z.object({
+          name: z.string(),
+          email: z.string().email()
+        })
+      }
+    }
+  }, async (request, reply) => {
+    const { name, email } = request.body
+
+    // criação da inscrição no banco de dados
+
+    console.log('POST /subscribers', request.body)
+    return reply.status(201).send({
+      name,
+      email
+    })
+  })
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -6,9 +6,9 @@ import {
   ZodTypeProvider,
   jsonSchemaTransform
 } from 'fastify-type-provider-zod'
-import { z } from 'zod'
 import { fastifySwagger } from '@fastify/swagger'
 import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -33,35 +33,12 @@ app.register(fastifySwaggerUi, {
   routePrefix: '/docs'
 })
 
-app.post('/subscribers', {
-  schema: {
-    body: z.object({
-      name: z.string(),
-      email: z.string().email()
-    }),
-    response: {
-      201: z.object({
-        name: z.string(),
-        email: z.string().email()
-      })
-    }
-  }
-}, async (request, reply) => {
-  const { name, email } = request.body
-
-  // criação da inscrição no banco de dados
-
-  console.log('POST /subscribers', request.body)
-  return reply.status(201).send({
-    name,
-    email
-  })
-})
-
 app.get('/healthcheck', () => {
   console.log('GET 200 /healthcheck')
   return 'Ok'
 })
+
+app.register(subscribeToEventRoute)
 
 app.listen({ port: 3000 }).then(() => {
   console.log('🚀 HTTP server is running!', 'http://127.0.0.1:3000')
